a03ab9c1e68cda96c0af4a8bcf637c36fcadd072
James Carter <136309778+JamesC215@users.noreply.github.com>
2023-06-26 11:03:59 +0100

    Add alphabet function JS, add styles CSS 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+initialise () {
 /*---------------- constants --------------*/
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+        let categories;         // Array of topics
+        let chosenCategory;     // Selected catagory
+        var getHint ;           // Word getHint
+        let word ;              // Selected word
+        let guess ;             // Player Guess
+        let guesses = [ ];      // Stored geusses
+        let lives ;             // Lives
+        let counter ;           // Count correct guesses
+        let space;              // Number of spaces in word '-'
 
-        
 /*------------ app's state (variables) -----------*/
 
 
@@ -12,7 +21,10 @@ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
 /*----------- cached element references ----------*/
 
-
+  let showLives = document.getElementById("mylives");
+  let showCategory = document.getElementById("scatagory");
+  let getHint = document.getElementById("hint");
+  let showClue = document.getElementById("clue");
 
 
 /*------------ event listeners -----------*/
@@ -21,3 +33,18 @@ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
 
 /*--------------- functions -----------*/
+// create alphabet ul
+ let buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (let i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+}
