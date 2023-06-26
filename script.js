29927c80e61356ae434ff3a3950ce9b01b354513
James Carter <136309778+JamesC215@users.noreply.github.com>
2023-06-26 16:23:58 +0100

    made an error 	modified:   script.js

--- script.js
+++ script.js
@@ -4,28 +4,32 @@ let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-        let categories;         // Array of topics
-        let chosenCategory;     // Selected catagory
-        var getHint ;           // Word getHint
-        let word ;              // Selected word
-        let guess ;             // Player Guess
-        let guesses = [ ];      // Stored geusses
-        let lives ;             // Lives
-        let counter ;           // Count correct guesses
-        let space;              // Number of spaces in word '-'
+/*------------ event listeners -----------*/
+let categories;         // Array of topics
+let chosenCategory;     // Selected catagory
+var getHint ;           // Word getHint
+let word ;              // Selected word
+let guess ;             // Player Guess
+let guesses = [ ];      // Stored geusses
+let lives ;             // Lives
+let counter ;           // Count correct guesses
+let space;              // Number of spaces in word '-'
 
 /*------------ app's state (variables) -----------*/
 
 const buttonsContainer = document.getElementById('buttons');
 
-
 /*----------- cached element references ----------*/
 
-
-
+alphabet.forEach(letter => {
+    const button = document.createElement('button');
+    button.textContent = letter;
+    button.addEventListener('click', () => handleButtonClick(letter));
+    buttonsContainer.appendChild(button);
+  });
 /*------------ event listeners -----------*/
 
-
-
-
 /*--------------- functions -----------*/
+
+function handleButtonClick(letter) {
+}
